c57e8f40d8c7426a0bd14059ae5a6e5c8791a932
Lenovo <jannatjui414@gmail.com>
2024-12-23 18:06:51 +0600

    make api for submission assignment

--- index.js
+++ index.js
@@ -118,6 +118,14 @@ app.delete('/assignments/:id', async(req, res)=>{
     })
 
 
+    // get pending assignment by spcific id
+    app.get('/submittedAssignments/:id', async(req, res)=>{
+      const id =req.params.id
+      const query ={_id: new ObjectId(id)}
+      const result =await submissionCollection.findOne(query)
+      res.send(result)
+      })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
